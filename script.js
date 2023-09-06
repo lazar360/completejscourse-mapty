42838763c5b732fa99d8861af828391761a9876c
NicolasGautier <nicolas.gautier35135@gmail.com>
2023-09-06 15:28:03 +0200

    maj script

--- script.js
+++ script.js
@@ -11,57 +11,108 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
+
+  constructor(coords, distance, duration) {
+    this.coords = coords; // [lat, long]
+    this.distance = distance; // in km
+    this.duration = duration; // in min
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace(){
+    // min/km
+    this.pace = this.duration / this.distance;
+    return this.pace
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+  }
+  
+  calcSpeed(){
+    // km/h
+    this.speed = this.distance / (this.duration/60)
+    return this.speed;
+  }
+}
+
+// const run1 = new Running([39, -12], 5.2, 24, 178);
+// const cycling1 = new Cycling([39, -12], 27, 95, 523);
+// console.log(run1, cycling1);
+////////////////////////////
+// APPLICATION ARCHITECTURE
 class App {
   #map;
   #mapEvent;
 
-  constructor(){
+  constructor() {
     this._getPosition();
     form.addEventListener('submit', this._newWorkout.bind(this));
 
-    inputType.addEventListener('change', this._toggleElevationField)
+    inputType.addEventListener('change', this._toggleElevationField);
   }
 
-  _getPosition(){
+  _getPosition() {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), function () {
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
           alert('Could not get your position');
         }
       );
     }
   }
 
-  _loadMap(position){
-      const { latitude } = position.coords;
-      const { longitude } = position.coords;
-      const coords = [latitude, longitude];
+  _loadMap(position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    const coords = [latitude, longitude];
 
-      this.#map = L.map('map').setView(coords, 13);
+    this.#map = L.map('map').setView(coords, 13);
 
-      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors',
-      }).addTo(this.#map);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
 
-      this.#map.on('click', this._showForm.bind(this));
+    this.#map.on('click', this._showForm.bind(this));
   }
-  
-  _showForm(mapE){
+
+  _showForm(mapE) {
     this.#mapEvent = mapE;
-        form.classList.remove('hidden');
-        inputDistance.focus();
+    form.classList.remove('hidden');
+    inputDistance.focus();
   }
 
-  _toggleElevationField(){ inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');}
- 
-  _newWorkout(e){
+  _toggleElevationField() {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+
+  _newWorkout(e) {
     e.preventDefault();
-    
-    inputCadence.value = inputDistance.value = inputDuration.value = inputElevation.value = '';
-    
+
+    inputCadence.value =
+      inputDistance.value =
+      inputDuration.value =
+      inputElevation.value =
+        '';
+
     const { lat, lng } = this.#mapEvent.latlng;
-  
+
     L.marker([lat, lng])
       .addTo(this.#map)
       .bindPopup(
@@ -79,5 +130,3 @@ class App {
 }
 
 const app = new App();
-
-
