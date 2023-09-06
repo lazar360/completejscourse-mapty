75bf400364d15013a2d3e42217ad4b302339b7f7
NicolasGautier <nicolas.gautier35135@gmail.com>
2023-09-06 10:21:14 +0200

    maj script

--- script.js
+++ script.js
@@ -14,7 +14,8 @@ const inputElevation = document.querySelector('.form__input--elevation');
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     function (position) {
-      console.log(position);
+      const {latitude} = position.coords;
+      const {longitude} = position.coords;
     },
     function () {
       alert('Could not get your position');
