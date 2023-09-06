84af918ff84904f5e20b9f5ba6bf00949ae106bc
NicolasGautier <nicolas.gautier35135@gmail.com>
2023-09-06 10:57:18 +0200

    add map

--- script.js
+++ script.js
@@ -14,8 +14,21 @@ const inputElevation = document.querySelector('.form__input--elevation');
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     function (position) {
-      const {latitude} = position.coords;
-      const {longitude} = position.coords;
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      const coords =[latitude, longitude]
+
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
     },
     function () {
       alert('Could not get your position');
